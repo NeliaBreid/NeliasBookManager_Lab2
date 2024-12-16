80da73d15f40d5ec8686fd677d3f8b3e9946faf3
NeliaBreid <nelia.Breid@gmail.com>
2024-12-16 18:21:33 +0100

    Made the add and remove Book function works a bit better

--- NeliasBookManager/Viewmodel/MainWindowViewModel.cs
+++ NeliasBookManager/Viewmodel/MainWindowViewModel.cs
@@ -1,17 +1,9 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Collections.ObjectModel;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Collections.ObjectModel;
 using BookButikAdminLab2.Command;
-using Labb2DataAcess.Services;
-using Microsoft.EntityFrameworkCore;
 using NeliasBookManager.Domain.ModelsDb;
 using NeliasBookManager.Infrastructure.Data;
 using NeliasBookManager.presentation.Dialogs;
 using NeliasBookManager.presentation.Models;
-using static System.Reflection.Metadata.BlobBuilder;
 
 namespace NeliasBookManager.presentation.Viewmodel
 {
@@ -27,16 +19,15 @@ namespace NeliasBookManager.presentation.Viewmodel
 
         private BookModel? _activeBook;
 
-        private int? _quantityToAdd;
-        public int? QuantityToAdd
+        private int? _quantityToAdd = 0;
+        public int? QuantityToAlter
         {
             get => _quantityToAdd;
             set
             {
-                _quantityToAdd = value;
+                if (value < 0) _quantityToAdd = 0;
+                else _quantityToAdd = value;
                 RaisePropertyChanged(nameof(ActiveStore));
-                GetBooksInStore();
-
             }
         }
         public BookModel? ActiveBook
@@ -46,8 +37,6 @@ namespace NeliasBookManager.presentation.Viewmodel
             {
                 _activeBook = value;
                 RaisePropertyChanged();
-
-                //TODO: Varje gång man byter bok så uppdateras boken? eller sätta propertychanged
             }
         }
         public StoreModel? ActiveStore
@@ -57,12 +46,9 @@ namespace NeliasBookManager.presentation.Viewmodel
             {
                 _activeStore = value;
                 BooksInStore.Clear();
-                TestGetBooksInStore(ActiveStore.Id);
+                GetBooksInActiveStore(ActiveStore.Id);
                 RaisePropertyChanged(nameof(ActiveStore));
                 RaisePropertyChanged(nameof(BooksInStore));
-
-               
-
             }
         }
         public ObservableCollection<BookModel> BooksInStore
@@ -75,7 +61,7 @@ namespace NeliasBookManager.presentation.Viewmodel
             }
         }
 
-        public RelayCommand OpenAddBooksCommand { get; }
+        public RelayCommand OpenAddBooksDialogCommand { get; }
         public RelayCommand OpenRemoveBooksDialogCommand { get; }
         public RelayCommand AddBookToStoreCommand { get; }
         public RelayCommand RemoveBookFromStoreCommand { get; }
@@ -85,164 +71,130 @@ namespace NeliasBookManager.presentation.Viewmodel
             BooksInStore = new ObservableCollection<BookModel>();
             AllBooks = new ObservableCollection<BookModel>();
             Authors = new ObservableCollection<AuthorModel>();
-            OpenAddBooksCommand = new RelayCommand(OpenAddBookDialog);
+            OpenAddBooksDialogCommand = new RelayCommand(OpenAddBookDialog);
             OpenRemoveBooksDialogCommand = new RelayCommand(OpenRemoveBooksDialog);
             AddBookToStoreCommand = new RelayCommand(AddBookToSaldo);
-            RemoveBookFromStoreCommand = new RelayCommand(RemoveBookFromStore);
+            RemoveBookFromStoreCommand = new RelayCommand(RemoveBookFromStoreSaldo);
 
             LoadStores(); //den här ska vara async sen
             
         }
-       //private bool CanAddBookToSaldo(object? arg)
-       // {
-       //     return ActiveBook != null && ActiveStore != null;
-       // }
+
         private void AddBookToSaldo(object obj)
         {
-        //    var newBook = new BookModel() //Skapar en ny bok som är en kopia av activebook
-        //    {
-        //        Isbn13 = ActiveBook.Isbn13,
-        //        Title = ActiveBook.Title,
-        //        Price = ActiveBook.Price,
-        //        PublishingDate = ActiveBook.PublishingDate,
-        //        Authors = new ObservableCollection<AuthorModel>(
-        //          ActiveBook.Authors.Select(a => new AuthorModel
-        //          {
-        //              Id = a.Id,
-        //              FirstName = a.FirstName,
-        //              LastName = a.LastName
-        //          })
-        //      ),
-        //        AmountInStore = new ObservableCollection<InventoryBalanceModel>
-        //{
-        //    new InventoryBalanceModel
-        //    {
-        //        Isbn13 = ActiveBook.Isbn13,
-        //        Quantity = QuantityToAdd,
-        //        StoreId = ActiveStore.Id,
-        //        Store = ActiveStore,
-        //        Isbn13Navigation = ActiveBook
-        //    }
-        //}
-        //    };
-
-            using var context = new NeliasBokHandelContext();
-            var existingSaldo = ActiveStore.IventoryBalances.FirstOrDefault(ib => ib.Isbn13 == ActiveBook.Isbn13);
-
-            var bookInStore = BooksInStore.FirstOrDefault(b => b.Isbn13 == ActiveBook.Isbn13); //kolla om boken finns
-
-
-            if (ActiveBook == null || QuantityToAdd <= 0) //Om det inte finns någon bok eller kvantiteten är underlikamed noll
+            if (ActiveBook != null)
             {
-                return;
-            }
-            else if (bookInStore == null)
-            {
-                //AddNewBookAndSaldo(newBook);
-            }
-            else
-            {
-                existingSaldo.Quantity += QuantityToAdd;
-                UpdateExistingSaldo(existingSaldo);// Uppdaterar saldot om den har ett saldot finns
-            }
+                var existingSaldo = ActiveStore.IventoryBalances.FirstOrDefault(ib => ib.Isbn13 == ActiveBook.Isbn13);
+                var bookInStore = BooksInStore.FirstOrDefault(b => b.Isbn13 == ActiveBook.Isbn13); //kolla om boken finns
 
+                if (QuantityToAlter <= 0 || QuantityToAlter == null) //Om det inte finns någon bok eller kvantiteten är underlikamed noll
+                {
+                    return;
+                }
+                else if (bookInStore == null) //om boken inte redan finns
+                {
+                    AddNewBookToDataBase();
+                }
+                else
+                {
+                    existingSaldo.Quantity += QuantityToAlter;
+                    UpdateExistingSaldo(existingSaldo);// Uppdaterar saldot om den har ett saldot finns
+                }
+            }
         }
         private void UpdateExistingSaldo(InventoryBalanceModel existingSaldo) //uppdaterar saldo, om det redan finns ett saldo
         {
-            if (existingSaldo != null)
+            using var context = new NeliasBokHandelContext();
+
+            var dbSaldo = context.LagerSaldos
+                .FirstOrDefault(ls => ls.Isbn == existingSaldo.Isbn13 && ls.ButikId == ActiveStore.Id);
+
+            if (dbSaldo != null)
             {
-                using var context = new NeliasBokHandelContext();
-
-                // Hämta rätt saldo från databasen
-                var dbSaldo = context.LagerSaldos
-                    .FirstOrDefault(ls => ls.Isbn == existingSaldo.Isbn13 && ls.ButikId == ActiveStore.Id);
-
-                if (dbSaldo != null)
-                {
-                    // Uppdatera både det lokala objektet och databasen
-                   // existingSaldo.Quantity += QuantityToAdd;
-                    dbSaldo.AntalBöcker = existingSaldo.Quantity;
-                    context.SaveChanges();
-                    GetBooksInStore();
-                    TestGetBooksInStore(dbSaldo.ButikId);
-                    RaisePropertyChanged(nameof(BooksInStore)); //kanske är överflödigt?
-                }
+                dbSaldo.AntalBöcker = existingSaldo.Quantity;
+                context.SaveChanges();
+                GetBooksInActiveStore(dbSaldo.ButikId); //hämtar böckerna
             }
-            
         }
-        //private void AddNewBookAndSaldo(BookModel book)
-        //{
-        //    using var context = new NeliasBokHandelContext();
-
-        //    var newInventoryBalance = new InventoryBalanceModel
-        //    {
-        //        Isbn13 = book.Isbn13,
-        //        Quantity = QuantityToAdd,
-        //        StoreId = ActiveStore.Id,
-        //        Store = ActiveStore
-        //    };
-
-        //    var newBookDb = new Böcker
-        //    {
-        //        Isbn = book.Isbn13,
-        //        Titel = book.Title,
-        //        Pris = book.Price,
-        //        Utgivningsår = book.PublishingDate
-        //    };
-
-        //    var newSaldoDb = new LagerSaldo
-        //    {
-        //        Isbn = book.Isbn13,
-        //        ButikId = ActiveStore.Id,
-        //        AntalBöcker = QuantityToAdd
-        //    };
-
-        //    // Lägg till i context
-        //    context.Böckers.Add(newBookDb);
-        //    context.LagerSaldos.Add(newSaldoDb);
-        //    //context.SaveChanges();
-
-        //    // Uppdatera lokalt
-        //    ActiveStore.IventoryBalances.Add(newInventoryBalance);
-        //    BooksInStore.Add(book);
-        //    RaisePropertyChanged(nameof(BooksInStore));
-        //}
-        private void RemoveBookFromStore(object obj)
+        private void AddNewBookToDataBase()
         {
             using var context = new NeliasBokHandelContext();
 
-            // Hämta existerande saldo och bok
-            var existingSaldo = ActiveStore.IventoryBalances.FirstOrDefault(ib => ib.Isbn13 == ActiveBook.Isbn13);
-            var bookInStore = BooksInStore.FirstOrDefault(b => b.Isbn13 == ActiveBook.Isbn13);
-
-            // Validering av input
-            if (ActiveBook == null || QuantityToAdd <= 0 || existingSaldo == null || bookInStore == null)
+            var newSaldoDb = new LagerSaldo
             {
-                return; 
-            }
+                Isbn = ActiveBook.Isbn13,
+                ButikId = ActiveStore.Id,
+                AntalBöcker = QuantityToAlter
+            };
 
-            // Uppdatera saldot
-            if (bookInStore != null)
+            var newInventoryBalance = new InventoryBalanceModel
             {
-                existingSaldo.Quantity -= QuantityToAdd;
-                UpdateExistingSaldo(existingSaldo);
-            }
-            //else
-            //{
-            //    // Om kvantiteten når 0, ta bort boken
-            //    ActiveStore.IventoryBalances.Remove(existingSaldo);
-            //    BooksInStore.Remove(bookInStore);
-            //    context.LagerSaldos.Remove(
-            //        context.LagerSaldos.SingleOrDefault(ls => ls.Isbn == ActiveBook.Isbn13 && ls.ButikId == ActiveStore.Id));
-            //    context.Böckers.Remove(
-            //        context.Böckers.SingleOrDefault(b => b.Isbn == ActiveBook.Isbn13));
+                Isbn13 = ActiveBook.Isbn13,
+                Quantity = QuantityToAlter,
+                StoreId = ActiveStore.Id,
+                Store = ActiveStore
+            };
 
-            //    RaisePropertyChanged(nameof(BooksInStore));
-            //}
+            ActiveStore.IventoryBalances.Add(newInventoryBalance);
 
-            // Spara ändringar i databasen
+            context.LagerSaldos.Add(newSaldoDb);
+            context.SaveChanges();
 
+            GetBooksInActiveStore(ActiveStore.Id);
         }
+        private void RemoveBookFromStoreSaldo(object obj)
+        {
+            if (ActiveBook != null)
+            {
+                var existingSaldo = ActiveStore.IventoryBalances.FirstOrDefault(ib => ib.Isbn13 == ActiveBook.Isbn13);
+               // var bookInStore = BooksInStore.FirstOrDefault(b => b.Isbn13 == ActiveBook.Isbn13);
+
+                // Validering av input
+                if (QuantityToAlter <= 0 || QuantityToAlter == null || existingSaldo == null) //Om det inte finns någon bok eller kvantiteten är underlikamed noll
+                {
+                    return; //fångar om booken inte finns i butiken, om man inte väljer kvantitet
+                }
+
+                else if (existingSaldo.Quantity - QuantityToAlter == 0) //om boken inte redan finns
+                {
+                    RemoveBookfromDataBase();
+                }
+                else if (existingSaldo.Quantity - QuantityToAlter >= 0)//inte kunna ta minusvärden
+                {
+                    existingSaldo.Quantity -= QuantityToAlter;
+                    UpdateExistingSaldo(existingSaldo);
+                }
+
+            }
+        }
+
+        public void RemoveBookfromDataBase()
+        {
+            using var context = new NeliasBokHandelContext();
+
+            var newSaldoDb = new LagerSaldo
+            {
+                Isbn = ActiveBook.Isbn13,
+                ButikId = ActiveStore.Id,
+                AntalBöcker = QuantityToAlter
+            };
+            var newInventoryBalance = new InventoryBalanceModel
+            {
+                Isbn13 = ActiveBook.Isbn13,
+                Quantity = QuantityToAlter,
+                StoreId = ActiveStore.Id,
+                Store = ActiveStore
+            };
+
+            ActiveStore.IventoryBalances.Remove(newInventoryBalance);
+
+            context.LagerSaldos.Remove(newSaldoDb);
+            context.SaveChanges();
+
+            GetBooksInActiveStore(ActiveStore.Id);
+        }
+
+        
         private void OpenAddBookDialog(object obj)
         {
             AddBooksDialog createNewDialog = new AddBooksDialog();
@@ -257,10 +209,9 @@ namespace NeliasBookManager.presentation.Viewmodel
             createNewDialog.ShowDialog();
 
         }
-        public void LoadStores() //laddar in alla butiker och lägger dom i Stores
+        public void LoadStores() //laddar in alla butiker och lägger dom i Stores, sätt dit en await
         {
             using var context = new NeliasBokHandelContext();
-
 
             var storesFromDb = context.Butikers.Select(
                 s => new StoreModel()
@@ -314,45 +265,10 @@ namespace NeliasBookManager.presentation.Viewmodel
             AllBooks = new ObservableCollection<BookModel>(books);
 
         }
-        public void GetBooksInStore() //TODO:lägga typ allBooks och något kriterie på butikID
+        
+        public void GetBooksInActiveStore(int storeId) //TODO:lägga typ allBooks och något kriterie på butikID
         {
             using var context = new NeliasBokHandelContext();
-
-            var CurrentStoreId = ActiveStore.Id;
-
-            var BooksPerStore = context.Böckers.Where(b => b.LagerSaldos.Any(ls => ls.ButikId == CurrentStoreId)).
-                Select(
-                b => new BookModel()
-                {
-                    Isbn13 = b.Isbn,
-                    Title = b.Titel,
-                    Price = b.Pris,
-                    PublishingDate = b.Utgivningsår,
-                    Authors = new ObservableCollection<AuthorModel>(
-                        b.Författars.Select(a => new AuthorModel()
-                        {
-                            Id = a.FörfattarId,
-                            FirstName = a.Förnamn,
-                            LastName = a.Efternamn
-                        }).ToList()),
-                    AmountInStore = new ObservableCollection<InventoryBalanceModel>(
-                b.LagerSaldos.Select(ls => new InventoryBalanceModel()
-                {
-                    StoreId = ls.ButikId,
-                    Quantity = ls.AntalBöcker,
-                    Isbn13 = ls.Isbn
-                }).ToList()
-                )
-                
-                }).ToList();
-            
-            BooksInStore = new ObservableCollection<BookModel>(BooksPerStore);
-        }
-        public void TestGetBooksInStore(int storeId) //TODO:lägga typ allBooks och något kriterie på butikID
-        {
-            using var context = new NeliasBokHandelContext();
-
-
 
             var BooksPerStore = context.Böckers.Where(b => b.LagerSaldos.Any(ls => ls.ButikId == storeId)).
                 Select(
@@ -387,5 +303,6 @@ namespace NeliasBookManager.presentation.Viewmodel
 
 
     }
+
 }
 
